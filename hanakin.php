9ce4d42b1946c2142261cac9c27139db30de86a3
tobe <tobe@n-i-agroinformatics.com>
2014-10-03 12:13:45 +0900

    スクリプト on PHP 実装

--- hanakin.php
+++ hanakin.php
@@ -1,9 +1,53 @@
 <?php
 
-function hanakin(){
-	echo "今月はスーパー花金\n";
+$options = getopt("p:y:m:");
+
+$payday = $options["p"];
+$month = $options["m"];
+$year = $options["y"];
+
+function hanakin($payday, $month, $year){
+	$days = range(1, date("t", mktime(0, 0, 0, $month, 1, $year)));
+	
+	$buff = array();	
+	$buff[] = $year. "/". $month;
+	$buff[] = "=================";
+	
+	$superHanakin = false;
+	foreach($days as $day){
+		$tmp = array();
+		$tmp[] = $day;
+		$date = date("w", mktime(0, 0, 0, $month, $day, $year));
+		
+		if($date == 5 && $day == $payday){
+			$tmp[] = " FizzBuzz";
+			$superHanakin = true;
+		}else if($date == 5){
+			$tmp[] = " Fizz";
+		}else if($day == $payday){
+			$tmp[] = " Buzz";
+		}
+		
+		$buff[] = implode(" ", $tmp);
+	}
+	
+	
+	if($superHanakin)$buff[] = superHanakin();
+	
+	$buff[] = "";
+	echo implode("\n", $buff);
 }
 
-hanakin();
+function superHanakin(){
 
+	return <<<SUPER_HANAKIN
+	
+＿人人人人人人人人人＿
+＞今月はスーパー花金＜
+￣Ｙ^Ｙ^Ｙ^Ｙ^Ｙ^Ｙ￣
+
+SUPER_HANAKIN;
+}
+
+hanakin($payday, $month, $year);
 ?>
